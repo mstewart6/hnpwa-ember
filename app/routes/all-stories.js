b844e8c0b52dcfffb69a4ecf9ebff8d46e416968
Michael Stewart <th3on3andonlyon3@gmail.com>
2019-03-11 20:27:55 -0500

    Add fastboot

--- app/routes/all-stories.js
+++ app/routes/all-stories.js
@@ -1,8 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service'
 import fetch from 'fetch';
 
 export default Route.extend({
   host: 'https://api.hackerwebapp.com',
+  fastboot: service(),
 
   queryParams: {
     page: {
@@ -13,9 +15,30 @@ export default Route.extend({
   async model(params) {
     let page = params.page;
     let type = this.get('type');
+    let shoebox = this.get('fastboot.shoebox');
+    let storeName = 'hnpwa-shoebox';
+    let shoeboxStore = shoebox.retrieve(storeName);
+    let key = `${type}-${page}`;
 
-    let response = await fetch(`${this.get('host')}/${type}?page=${page}`);
+    if (this.get('fastboot.isFastBoot')) {
+      let response = await fetch(`${this.get('host')}/${type}?page=${page}`);
+      let data = await response.json();
 
-    return await response.json();
+      //
+      if (!shoeboxStore) {
+        shoeboxStore = {};
+        shoebox.put(storeName, shoeboxStore);
+      }
+
+      return shoeboxStore[key] = data;
+    } else {
+      // Load data from shoebox, if present, otherwise fetch
+      if (shoeboxStore && shoeboxStore[key]) {
+        return shoeboxStore[key];
+      } else {
+        let response = await fetch(`${this.get('host')}/${type}?page=${page}`);
+        return await response.json();
+      }
+    }
   }
 });
